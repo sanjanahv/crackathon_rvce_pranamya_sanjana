dad47bd58878d95f50b77f8604bb353bdd40d0c8
sanjanahv <7sanjanarvce@gmail.com>
2026-01-09 22:56:54 +0530

    Implement model training with YOLO and config

---
+++ train.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+import yaml
+
+# Load dataset config
+DATA_YAML = "dataset.yaml"
+
+# Load existing weights for refinement
+OLD_WEIGHTS_PATH = "/kaggle/input/crackathon-rvce/runs/detect/train/weights/best.pt"
+
+model = YOLO(OLD_WEIGHTS_PATH)
+
+# Train / refine model
+model.train(
+    data=DATA_YAML,
+    epochs=100,
+    imgsz=800,
+    batch=-1,
+    patience=20,
+    optimizer="AdamW",
+    project="crackathon_final",
+    name="v8s_refined"
+)
